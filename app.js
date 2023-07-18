15a62725bda515187dcb6bacd77ab2972659f9e8
appworld-hu <burai.sandor@gmail.com>
2023-07-19 01:43:09 +0200

    validation

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
-const express = require('express');
- 
-const app = express();
- 
-//app.use(express.json());       
-app.use(express.urlencoded({extended: true})); 
- 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+const express = require('express') // Include ExpressJS
+const app = express() // Create an ExpressJS app
+const bodyParser = require('body-parser'); // Middleware
+
+
+// Include Express Validator Functions
+const { check, validationResult } = require('express-validator');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// Homepage
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/static/index.html');
 });
- 
-app.post("/", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  console.log("Username: " + username);
-  console.log("Password: " + password);
-  res.send("Data received: "+`${username} - ${password}`);
+// Login Page
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/static/login.html');
 });
- 
-app.listen(3000);
+
+// Validation Array
+var loginValidate = [
+  // Check Username
+  check('username', 'Username Must Be an Email Address').isEmail()
+  .trim().escape().normalizeEmail(),
+  // Check Password
+  check('password').isLength({ min: 8 }).withMessage('Password Must Be at Least 8 Characters').matches('[0-9]').withMessage('Password Must Contain a Number').matches('[A-Z]').withMessage('Password Must Contain an Uppercase Letter').trim().escape()];
+// Process User Input
+
+app.post('/login', loginValidate, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+  	return res.status(422).json({ errors: errors.array() });
+  }
+  else {
+ // Insert Login Code Here
+ let username = req.body.username;
+ let password = req.body.password;
+ res.send(`Username: ${username} Password: ${password}`);
+  }
+});
+const port = 3000// Port we will listen on
+// Function to listen on the port
+app.listen(port, () => console.log(`This app is listening on port ${port}`));
